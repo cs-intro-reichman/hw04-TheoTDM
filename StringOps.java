72c8594151a3aa8dce15cdeae5dc7ace44e0c5ee
TheoTDM <theodore.daimaman@post.runi.ac.il>
2024-01-16 00:20:48 +0200

    8

--- StringOps.java
+++ StringOps.java
@@ -12,14 +12,14 @@ public class StringOps {
         for(int i = 0 ; i<s.length() ; i++){
             test =false;
             char ch = s.charAt(i);
-            if(ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch ==117 ){
+            if(ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch == 117){
                 ans = ans + (char)(s.charAt(i)-32);
                 test = true;
             }
-            if(ch > 65 && ch != 69 && ch != 73 && ch != 79 && ch != 80 && ch <= 90){
-                ans = ans +(char)(s.charAt(i)+32);
-            else if(ch >= 65 && ch <= 90){
-                ans = ans + (char)s.charAt(i);
+            if(ch > 65 && ch <= 90 && ch != 69 && ch != 73 && ch != 79 && ch != 80){
+                ans = ans +(char)(s.charAt(i) + 32);
+                if(ch >= 65 && ch <= 90){
+                    ans = ans + (char)s.charAt(i);
                 }
             else {
                 if(test == false)
